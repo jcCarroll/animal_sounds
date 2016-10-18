e2b2600bfe8018db0d70f8f56a87c28a552511ce
jacob <2staffi@gmail.com>
2016-10-18 11:29:21 -0400

    test cat, dog, and fox name

--- animal_test.rb
+++ animal_test.rb
@@ -10,4 +10,14 @@ class TestAnimalSounds <Minitest::Test
         assert_equal("Kitty", animal.name)
     end
 
+    def test_dog_name
+        animal = Dog.new("Doggy")
+        assert_equal("Doggy", animal.name)
+    end
+
+    def test_fox_name
+        animal = Fox.new("Foxy")
+        assert_equal("Foxy", animal.name)
+    end
+
 end
