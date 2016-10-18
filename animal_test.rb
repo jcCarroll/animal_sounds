8174f6ef34c1d87793b4dc703980664be72370f5
jacob <2staffi@gmail.com>
2016-10-18 10:54:34 -0400

    setup first test

---
+++ animal_test.rb
@@ -0,0 +1,11 @@
+require "minitest/autorun"
+require_relative "cat.rb"
+require_relative "dog.rb"
+require_relative "fox.rb"
+
+class TestAnimalSounds <Minitest::Test
+
+    def test_cat_name
+    end
+
+end
