c590c2300777433c0586fc4f2321d4f7f8b303cc
jacob <2staffi@gmail.com>
2016-10-18 10:57:16 -0400

    animal.name returns Marv

--- animal_test.rb
+++ animal_test.rb
@@ -6,6 +6,8 @@ require_relative "fox.rb"
 class TestAnimalSounds <Minitest::Test
 
     def test_cat_name
+        animal = Cat.new("Marv")
+        assert_equal("Marv", animal.name)
     end
 
 end
